3b1d1e4d6c8f2554f73dfad550cabaf32c54d686
wuniu2010 <wuniu2010@126.com>
2017-03-22 15:21:24 +0800

    精简代码

--- src/js/testA.js
+++ src/js/testA.js
@@ -2,13 +2,17 @@
  * xuess
  * 测试JS A
  */
-//var dialog = require('dialog');
+var dialog = require('dialog');
+var artTemplate = require('artTemplate');
+var md5 = require('md5');
 //require('../css/testA.css');
-require('../css/testAless.less');
-require('../css/testAsass.scss');
+require('../css/testAless');
+require('../css/testAsass');
 
 $(function(){
-	console.log('this testA.js!!');
-//	console.log('dialog==',dialog);
+	console.log('this testA.js!!，is do it！！');
 	$('h1').html('jquery 修改了内容！！').hide('slow').show('slow');
+	console.log(artTemplate);
+	console.log(dialog);
+	console.log(md5('xxxx'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,14 @@
 var path = require('path'),
 	webpack = require('webpack'),
 	//打包html
-//	HtmlWebpackPlugin = require('html-webpack-plugin'),
+	//	HtmlWebpackPlugin = require('html-webpack-plugin'),
 	//独立打包样式文件
 	ExtractTextPlugin = require("extract-text-webpack-plugin"),
 	//公共模块单独打包
 	CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin,
-	ROOT_PATH = path.resolve(__dirname),//根目录
-	OUT_PATH = "./dist/",//输出目录
-	ALIAS_IGNORE_DIRS = [""],//忽略目录
+	ROOT_PATH = path.resolve(__dirname), //根目录
+	OUT_PATH = "./dist/", //输出目录
+	ALIAS_IGNORE_DIRS = [""], //忽略目录
 	ENTRY_IGNORE_DIRS = ["commons"], //忽略目录
 	alias = require('./config/alias'),
 	entry = require('./config/entry');
@@ -27,40 +27,43 @@ var sassLoader = extractCSS.extract(['css', 'sass'])
 var lessLoader = extractCSS.extract(['css', 'less'])
 //插件配置
 var plugins = [];
-plugins.push(extractCSS);
-///webpack.ProvidePlugin插件作用是jquery变成全局，所以不需要引用就能在js文件中运用
-plugins.push(new webpack.ProvidePlugin({
-	"jQuery": "jquery",
-	"$": "jquery"
-}));
-plugins.push(new CommonsChunkPlugin({
-	name: 'libs',
-	minChunks: Infinity
-}));
-plugins.push(new webpack.optimize.UglifyJsPlugin({
-	mangle: {
-		except: ['$', 'jQuery']
-	},
-	// sourceMap: false,
-	compress: {
-		warnings: false
-	}
-}));
+plugins.push(
+	extractCSS,
+	///webpack.ProvidePlugin插件作用是jquery变成全局，所以不需要引用就能在js文件中运用
+	new webpack.ProvidePlugin({
+		"jQuery": "jquery",
+		"$": "jquery"
+	}),
+	//打包公用库
+	new CommonsChunkPlugin({
+		name: 'libs',
+		minChunks: Infinity
+	}),
+	//混淆压缩，忽略$ jQuery
+	new webpack.optimize.UglifyJsPlugin({
+		mangle: {
+			except: ['$', 'jQuery']
+		},
+		// sourceMap: false,
+		//警告 false=不打印
+		compress: {
+			warnings: false
+		}
+	})
+);
 
 module.exports = {
-//	devtool: "source-map",
+	//	devtool: "source-map",
 	resolve: {
 		//参数名的自动补全，现在可以写 require('file') 代替 require('file.js')
-		extensions: ['', '.js', '.json', '.coffee', '.css', '.jsx', '.vue'],
+		extensions: ['', '.js', '.json', '.coffee', '.css', '.jsx', '.vue', '.less' ,'.scss'],
 		//解析目录名的一个数组到当前目录以及先前的目录，并且是查找模块。
-		modulesDirectories: ['node_modules'],
+//		modulesDirectories: ['node_modules'],
 		//设置别名，便于在开发中模块的使用
 		alias: alias.getAlias({
-			jquery: path.resolve(ROOT_PATH, "./node_modules/jquery/dist/jquery.min.js"),
-			md5: path.resolve(ROOT_PATH, "./node_modules/md5/md5.js")
-		}),
-		//绝对路径
-		publicPath: '/'
+//			jquery: ROOT_PATH + "/node_modules/jquery/dist/jquery.min.js",
+//			md5: path.resolve(ROOT_PATH, "./node_modules/md5/md5.js")
+		})
 	},
 	entry: entry.getEntrys({ 'libs': ['jquery','md5'] }),
 	output: {
